8771d9a1c74098e302a7bb2eb68fa54b0488157b
Victor Iturriago <victoritrurraigo@gmail.com>
2025-11-16 21:40:46 -0500

    Connecting DB - HTTP:GET

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -1,5 +1,17 @@
-const getCategorias =(req,res)=>{
-    res.json({"Categoria":" Electrodomesticos"})
+import { Connection } from "promise-mysql"
+import getConnection from "./../db/database.js"
+const getCategorias = async (req,res)=>{
+
+try {
+    const connection = await getConnection();
+    const result = await connection.query("SELECT CategoriaID,CategoriaNombre,Descripcion,Imagen FROM categorias")
+    res.json(result);
+} catch (error) {
+    console.error("ERROR 500")
+}
+    
+
+
 
 }
 
